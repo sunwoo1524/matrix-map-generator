ef9d987304e3c301566f32a70122863d49222c8a
맹선우 <maengseonu80@gmail.com>
2023-06-18 17:20:13 +0900

    edit title

--- src/components/app.js
+++ src/components/app.js
@@ -6,7 +6,7 @@ import Helmet from 'preact-helmet';
 
 const App = () => (
 	<div id="app">
-		<Helmet title="매트릭스 맵 메이커" />
+		<Helmet title="매트릭스 맵 생성기" />
 		<Router>
 			<Main path="/" />
 			<About path="/about" />
